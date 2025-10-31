7d80543cbe76537cbd2293a3971a2ce132a7bc50
misilelab <misileminecord@gmail.com>
2025-10-31 14:54:57 +0900

    fix(cof): Any missing error
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- src/cof/remote.py
+++ src/cof/remote.py
@@ -7,7 +7,7 @@ import asyncio
 import json
 import os
 from pathlib import Path
-from typing import Dict, List, Optional, Set
+from typing import Dict, List, Optional, Set, Any
 import click
 
 from cof.network import NetworkClient, CofProtocolError
